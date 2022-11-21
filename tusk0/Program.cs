3a4daad5e2b0abb2c914aaeae29259d7135b0d11
Mikhail_Golovkov <mik270400@mail.ru>
2022-11-21 19:16:13 +0300

    Task0Enablet

--- tusk0/Program.cs
+++ tusk0/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit1 = number/10;
+int digit2 = digit1%10;
+Console.WriteLine("Число: " + digit2);
